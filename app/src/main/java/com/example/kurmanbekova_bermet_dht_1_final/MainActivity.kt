073896df9c0d0debcdf27811c2750ca7ddb14163
bema13 <kurmanbekova_b@auca.kg>
2021-12-13 14:59:32 +0600

    bema

---
+++ app/src/main/java/com/example/kurmanbekova_bermet_dht_1_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kurmanbekova_bermet_dht_1_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
